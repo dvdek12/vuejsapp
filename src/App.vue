54f2ca58c0f45fb8605934eed9de9371992141fa
dvdek12 <przemekdudek321@wp.pl>
2021-10-19 17:28:19 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,113 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="w-screen h-screen flex justify-center">
+
+    <div class="flex flex-col space-y-6 items-center">
+      <div class="flex flex-col space-y-6 w-96">
+        <div class="inline-flex space-x-4 items-center">
+          <input type="text" class="w-auto h-auto p-2 rounded-md focus:outline-none ring-4 ring-indigo-900 ring-offset-2 ring-offset-indigo-500" 
+            placeholder="Search some news..."
+            v-model="search">
+
+            
+
+            <select v-model="language" class="focus:outline-none w-auto h-6 rounded-lg ring-2 ring-indigo-900 ring-offset-2 ring-offset-indigo-500">
+              <option class="text-black font-semibold">Arabic</option>
+              <option class="text-black font-semibold">German</option>
+              <option class="text-black font-semibold">English</option>
+              <option class="text-black font-semibold">Spanish</option>
+              <option class="text-black font-semibold">French</option>
+              <option class="text-black font-semibold">Italian</option>
+              <option class="text-black font-semibold">Russian</option>
+            </select>
+        </div>
+
+        
+        <button @click="getApiData()" class="w-auto h-auto p-5 bg-indigo-600 text-white rounded-xl">
+          Click To Get News!
+        </button>
+      </div>
+
+      <div v-if="show" class="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div v-for="news in arrayOfNews" :key="news">
+            <News :news="news"/>
+          </div>
+      </div>
+
+      <div v-if="noResults" class="w-auto p-5">
+          <p class="text-white fonmt-bold text-2xl">Sorry, We didn't find typed phrase..</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import News from './components/News.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+        arrayOfNews: [],
+        search: '',
+        language: '',
+        show: false,
+        noResults: false
+    }
+  },
+  methods: {
+    getApiData() {
+        var url = 'https://newsapi.org/v2/everything?' +
+          'q='+ this.search +'&' +
+          'from=2021-10-17&' +
+          'sortBy=popularity&' +
+          'language='+ this.convertLanguagesToISO(this.language) + '&' + 
+          'apiKey=6f7553412aea42819cde65451edff8b6';
+        
+        var req = new Request(url);
+
+        const news = fetch(req).then(function(response) {
+          var res = response.json();
+          return res;
+        })
+
+        const showNews = () => {
+          news.then((res) => {
+            if(res.articles.length !== 0) {
+              this.noResults = !this.noResults
+              this.arrayOfNews = res.articles;
+            }else{
+              this.show = false
+              this.noResults = true
+            }
+          });
+        };
+
+        showNews()
+        this.show = true;
+    },
+
+    convertLanguagesToISO(language) {
+        switch(language) {
+          case "Arabic":
+            return "ar";
+          case "German":
+            return "de";
+          case "English":
+            return "en";
+          case "Spanish":
+            return "es";
+          case "French":
+            return "fr";
+          case "Italian":
+            return "it";
+          case "Russian":
+            return "ru";
+        }
+    }
+  },
   components: {
-    HelloWorld
+    News
   }
 }
 </script>
---
+++ src/components/News.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="flex flex-col justify-center  w-96 bg-white rounded-lg" style="height: 45rem">
+        <div class="w-full h-1/3">
+            <img :src="news.urlToImage" alt="" class="w-full h-full rounded-lg">
+        </div>
+        <div class="relative w-full h-full flex flex-col items-between space-y-4">
+
+            <div class="flex flex-col items-start p-5 w-full h-auto space-y-6">
+                <p class="text-gray-400 text-sm font-bold">
+                  {{news.publishedAt.slice(0,10)}}
+                </p>
+
+                <h3 class="font-bold text-3xl text-gray-900 text-left">
+                  {{this.reduceTitle(news.title)}}
+                </h3>
+
+                <p class="text-gray-700 text-sm font-semibold text-left space-y-10 leading-loose">
+                  {{news.description}}
+                </p>
+            
+                
+            </div>
+
+            <div class="absolute -inset-x-24 bottom-0 w-full h-auto ">
+              <button class="text-purple-700  my-12 text-2xl font-semibold">
+                  Find out more
+              </button>
+            </div> 
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'News',
+  props: ['news'],
+  methods: {
+    reduceTitle(title) {
+        if(typeof String && title.length > 15){
+          return title.slice(0,50) + "..."
+        }
+    }
+
+    
+  }
+}
+
+
+</script>
+
+
